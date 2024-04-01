cbefc7df6a9b0d63b0a01f61a4bbd3a6fb724a3d
Patrick Krueger <sendthistopatrick@gmail.com>
2024-04-01 15:21:32 -0600

    sessions

--- layouts/acs.session.sh
+++ layouts/acs.session.sh
@@ -1,34 +1,24 @@
 # Set a custom session root path. Default is `$HOME`.
 # Must be called before `initialize_session`.
-session_root "~/code/kaleidoquill_app/"
+session_root "~/code/clients/acs/acs-nuxt-app/"
 
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
-if initialize_session "KaleidoQuill"; then
+if initialize_session "ACS"; then
 
   # Create a new window inline within session layout definition.
-  new_window "Compose"
-  run_cmd "dcupd"
-  sleep 1
-  new_window "Frontend"
-  run_cmd "cd kaleidoquill_frontend && nvim"
+  new_window "Nuxt App"
+  run_cmd "nvim"
   sleep 1
   send_keys " sf"
-  send_keys ""
-  new_window "Backend"
-  run_cmd "cd kaleidoquill_backend && nvim"
-  sleep 1
-  send_keys " sf"
-  new_window "Logs"
-  split_h 50
-  run_cmd "dclf kaleidoquill_frontend" 1
-  run_cmd "dclf kaleidoquill_backend" 2
 
   # Load a defined window layout.
   #load_window "example"
 
   # Select the default active window on session creation.
-  select_window "Compose"
+  #select_window 1
+
 fi
+
 # Finalize session creation and switch/attach to it.
 finalize_and_go_to_session
--- layouts/nvim-config.session.sh
+++ layouts/nvim-config.session.sh
@@ -1,34 +1,23 @@
 # Set a custom session root path. Default is `$HOME`.
 # Must be called before `initialize_session`.
-session_root "~/code/kaleidoquill_app/"
+session_root "~/.config/nvim/"
 
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
-if initialize_session "KaleidoQuill"; then
+if initialize_session "nvim-config"; then
 
   # Create a new window inline within session layout definition.
-  new_window "Compose"
-  run_cmd "dcupd"
-  sleep 1
-  new_window "Frontend"
-  run_cmd "cd kaleidoquill_frontend && nvim"
-  sleep 1
+  new_window "nvim"
+  run_cmd "nvim"
+  sleep 0.5
   send_keys " sf"
-  send_keys ""
-  new_window "Backend"
-  run_cmd "cd kaleidoquill_backend && nvim"
-  sleep 1
-  send_keys " sf"
-  new_window "Logs"
-  split_h 50
-  run_cmd "dclf kaleidoquill_frontend" 1
-  run_cmd "dclf kaleidoquill_backend" 2
-
   # Load a defined window layout.
   #load_window "example"
 
   # Select the default active window on session creation.
-  select_window "Compose"
+  #select_window 1
+
 fi
+
 # Finalize session creation and switch/attach to it.
 finalize_and_go_to_session
